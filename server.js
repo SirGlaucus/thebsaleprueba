0a91d87130b996d21e1f504b84eedae4fcbdf1cd
SirGlaucus <glaucusjoseph@gmail.com>
2022-07-06 14:51:37 -0400

    Ruta principal activa con handlebars activado

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Cargando librerias
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
+
+// Configuracion del handlebars 
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs.engine({ layoutsDir: __dirname + '/views' }))
 
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
@@ -10,7 +15,9 @@ app.use('/js', express.static(__dirname + '/assets/js'))
 
 app.get('/', (req, res) => {
     const ruta = path.join(__dirname, 'index.html')
-    res.sendFile(ruta)
+    res.render('index', {
+        layout: 'index'
+    })
 })
 
 
