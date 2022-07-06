265768531e4f99898d7c4a932e356d55da8b0b35
SirGlaucus <glaucusjoseph@gmail.com>
2022-07-06 16:53:45 -0400

    Navbar a otro lado, conexion sql creada

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const pool = require('./src/database')
 
 const app = express()
 
@@ -13,7 +14,9 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/c
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
 app.use('/js', express.static(__dirname + '/assets/js'))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const data = await pool.query('SELECT * FROM product')
+    console.log(data)
     res.render('index', {
         layout: 'index'
     })
